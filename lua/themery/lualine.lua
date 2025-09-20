cf1db9158b1842948b07aa825bf3c15522aed812
Bruno Da Silva <devbrunodasilva@gmail.com>
2025-09-20 11:28:12 -0300

    Update lualine.lua

--- lua/themery/lualine.lua
+++ lua/themery/lualine.lua
@@ -1,33 +1,33 @@
-local colors = require("themery.colors").palette
+local colors = require("purplewave.colors").palette
 
-local themery = {}
+local purplewave = {}
 
-themery.normal = {
+purplewave.normal = {
   a = { fg = colors.bg1, bg = colors.color2, gui = "bold" },
   b = { fg = colors.fg1, bg = "#2A2A2A" },
   c = { fg = colors.uic1, bg = "#1A1A1A" },
 }
 
-themery.insert = {
+purplewave.insert = {
   a = { fg = colors.bg1, bg = colors.color1, gui = "bold" },
 }
 
-themery.visual = {
+purplewave.visual = {
   a = { fg = colors.bg1, bg = colors.color6, gui = "bold" },
 }
 
-themery.replace = {
+purplewave.replace = {
   a = { fg = colors.bg1, bg = colors.color5, gui = "bold" },
 }
 
-themery.command = {
+purplewave.command = {
   a = { fg = colors.bg1, bg = colors.color3, gui = "bold" },
 }
 
-themery.inactive = {
+purplewave.inactive = {
   a = { fg = colors.uic1, bg = colors.bg1 },
   b = { fg = colors.uic1, bg = colors.bg1 },
   c = { fg = colors.uic1, bg = colors.bg1 },
 }
 
-return themery 
+return purplewave 
